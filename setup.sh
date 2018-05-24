69213f5ef9a639650b44b021af23abc14561d1e3
Eric Tan <eric@localhost.localdomain>
2018-05-23 18:49:21 -0700

    Added setup script

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+# Install necessary programs
+sudo dnf install util-linux-user zsh vim git
+
+# Setup vim
+mkdir vim/backup vim/swapfile vim/bundle
+ln -s "$(pwd)/vim" ~/.vim
+ln -s "$(pwd)/vim/vimrc" ~/.vimrc
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# Setup zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+rm ~/.zshrc
+ln -s "$(pwd)/zsh/zshrc" ~/.zshrc
