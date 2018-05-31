80b9fccf91d90eb92b2976008e46905e51e04f3f
Eric Tan <eric@localhost.localdomain>
2018-05-31 12:11:23 -0700

    Added link to dotfile/bin

--- setup.sh
+++ setup.sh
@@ -13,3 +13,6 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 rm ~/.zshrc
 ln -s "$(pwd)/zsh/zshrc" ~/.zshrc
+
+# Link directory of bin files
+ln -s "$(pwd)/bin" ~/bin
